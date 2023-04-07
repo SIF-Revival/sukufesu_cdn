66f6ddda3ab8b9370928670e70e564f571da8aba
RayFirefist <github@makoo.eu>
2023-04-07 16:14:30 +0200

    reworked cdn paths

--- config.py
+++ config.py
@@ -2,8 +2,7 @@ ADMIN_PASS = "admin_key" # CHANGE THIS KEY
 
 SANIC_HOST = "0.0.0.0"
 SANIC_PORT = 8080
-SANIC_DEBUG = False
+SANIC_DEBUG = True
 
-SIF_BASE_ASSET_PATH = "./assets/"
-SIF_BASE_BATCH_PATH = "./batch/"
+SIF_BASE_ASSET_PATH = "./archive-root/"
 SIF_BASE_DOMAIN_PATH = "http://localhost/"
--- routes.py
+++ routes.py
@@ -1,5 +1,5 @@
 # base imports
-import os
+import os as __os
 import hashlib
 import json as _json
 from wsgiref.handlers import format_date_time
@@ -13,6 +13,8 @@ from aiofiles import os as async_os
 
 # config import
 import config
+
+OS_ = ["Android", "iOS"]
 
 NOT_FOUND = json({
     "error": "FILE_NOT_FOUND",
@@ -39,19 +41,7 @@ async def test(request):
 async def getAssetFile(request, assetPath=""):
     print(assetPath)
     path = config.SIF_BASE_ASSET_PATH + assetPath
-    if os.path.exists(path):
-        return await file_stream(path, headers={
-            # "ETag": f"\"{etag}\"",
-            "Last-Modified": expireDate(False),
-            "Expires": expireDate()
-        })
-    else:
-        return NOT_FOUND
-
-async def getBatchFile(request, assetPath=""):
-    print(assetPath)
-    path = config.SIF_BASE_BATCH_PATH + assetPath
-    if os.path.exists(path):
+    if __os.path.exists(path):
         return await file_stream(path, headers={
             # "ETag": f"\"{etag}\"",
             "Last-Modified": expireDate(False),
@@ -61,27 +51,31 @@ async def getBatchFile(request, assetPath=""):
         return NOT_FOUND
 
 async def getAssetsList(request, os:str, version:str):
-    path = f"{config.SIF_BASE_BATCH_PATH}{version}/"
+    path = f"{config.SIF_BASE_ASSET_PATH}{os}/update/{version}/"
     print(path)
-    if os.path.exists(path):
+    if __os.path.exists(path):
         assetsList = []
 
-        scandir = os.scandir(path)
-        for file in scandir:
+        data = _json.load(open(f"{path}info.json", "r"))
+
+        for file, size in data.items():
+            __path = path.replace(config.SIF_BASE_ASSET_PATH, "")
             assetsList.append({
-                "size": os.path.getsize(f"{path}/{file.name}"),
-                "url": f"{config.SIF_BASE_DOMAIN_PATH}{path[2:]}{file.name}"
-            })
+                "size": size,
+                "url": f"{config.SIF_BASE_DOMAIN_PATH}assets/{__path}{file}"
+            })    
 
         return json({"response_data": assetsList, "status_code": 200})
     else:
         return NOT_FOUND
 
 async def getVersions(request):
-    versions = []
-    for path in os.scandir(config.SIF_BASE_BATCH_PATH):
-        if path.is_dir():
-            versions.append(path.name)
+    versions = {}
+    for _os in OS_:
+        versions[_os] = []
+        for path in __os.scandir(f"{config.SIF_BASE_ASSET_PATH}{_os}/update/"):
+            if path.is_dir():
+                versions[_os].append(path.name)
     return json({
         "response_data": {
             "versions": versions
@@ -92,17 +86,16 @@ def add_external_routes(app):
     # GET
     app.add_route(test, '/', methods=["GET"])
     app.add_route(getAssetFile, '/assets/', methods=["GET"])
-    app.add_route(getBatchFile, '/batch/', methods=["GET"])
     app.add_route(getAssetFile, '/assets/<assetPath:path>', methods=["GET"])
-    app.add_route(getBatchFile, '/batch/<assetPath:path>', methods=["GET"])
     app.add_route(getVersions, '/api/versions', methods=['GET'])
-    app.add_route(getAssetsList, '/api/getBatchAssets/<os:str>/<version:str>', methods=["GET"])
+    app.add_route(getAssetsList, '/api/getAssetsList/<os:str>/<version:str>', methods=["GET"])
 
     @app.middleware('response')
     async def print_on_response(request, response: sanic.response.BaseHTTPResponse):
         # print("I print when a response is returned by the server")
         #response.headers['Access-Control-Allow-Origin'] = "*"
         #response.headers['Access-Control-Allow-Methods'] = "GET, POST"
+        response.headers['Server'] = "sukufesu_cdn"
         pass
 
     @app.middleware('request')
